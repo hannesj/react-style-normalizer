e902cb115682b9c69591618929dd77bcc0afc9d6
radubrehar <radu@jslog.com>
2015-03-11 11:23:58 +0200

    update

--- src/el.js
+++ src/el.js
@@ -5,6 +5,8 @@ var el
 module.exports = function(){
 
 	console.log(global.document)
+	console.log(window)
+	console.log(document)
 
 	if(!el && !!global.document){
 	  	el = global.document.createElement('div')
