c37048a6d5c2c40ae73fbe89529b79adb763e53a
radubrehar <radu@jslog.com>
2015-03-11 11:38:06 +0200

    update

--- src/el.js
+++ src/el.js
@@ -4,12 +4,16 @@ var el
 
 module.exports = function(){
 
-	console.log(global.document)
-	console.log(window)
-	console.log(document)
+	// console.log(global.document)
+	// console.log(window)
+	// console.log(document)
 
 	if(!el && !!global.document){
 	  	el = global.document.createElement('div')
+	}
+
+	if (!el){
+		el = {style: {}}
 	}
 
 	return el
